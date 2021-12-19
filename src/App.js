4507aa3cd7400c9d20ee9bc0aa2cca30b29088cc
twomeetings <2326220@163.com>
2021-12-20 06:06:10 +0800

    chore: start relay (turns out error)

--- src/App.js
+++ src/App.js
@@ -2,49 +2,86 @@
 import React from 'react'
 import './App.css'
 import fetchGraphQL from './fetchGraphQL'
+import graphql from 'babel-plugin-relay/macro'
+import { RelayEnvironmentProvider, loadQuery, usePreloadedQuery } from 'react-relay/hooks'
+import RelayEnvironment from './RelayEnvironment'
 
-const { useState, useEffect } = React
-
-function App() {
-  // We'll load the name of a repository, initially setting it to null
-  const [name, setName] = useState(null)
-
-  // When the component mounts we'll fetch a repository name
-  useEffect(() => {
-    let isMounted = true
-    fetchGraphQL(`
-      query RepositoryNameQuery {
-        # feel free to change owner/name here
-        repository(owner: "facebook" name: "relay") {
-          name
-        }
-      }
-    `)
-      .then((response) => {
-        // Avoid updating state if the component unmounted before the fetch completes
-        if (!isMounted) {
-          return
-        }
-        const data = response.data
-        setName(data.repository.name)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-
-    return () => {
-      isMounted = false
+const { Suspense } = React
+const RepositoryNameQuery = graphql`
+  query AppRepositoryNameQuery {
+    repository(owner: "facebook", name: "relay") {
+      name
     }
-  }, [fetchGraphQL])
+  }
+`
 
-  // Render "Loading" until the query completes
+const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery, {})
+
+function App(props) {
+  const data = usePreloadedQuery(RepositoryNameQuery, props.preloadedQuery)
   return (
     <div className="App">
       <header className="App-header">
-        <p>{name != null ? `Repository: ${name}` : 'Loading'}</p>
+        <p>{data.repository.name}</p>
       </header>
     </div>
   )
 }
 
-export default App
+function AppRoot(props) {
+  return (
+    <RelayEnvironmentProvider enviroment={RelayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <App preloadedQuery={preloadedQuery}></App>
+      </Suspense>
+    </RelayEnvironmentProvider>
+  )
+}
+
+export default AppRoot
+
+// const { useState, useEffect } = React
+
+// function App() {
+//   // We'll load the name of a repository, initially setting it to null
+//   const [name, setName] = useState(null)
+
+//   // When the component mounts we'll fetch a repository name
+//   useEffect(() => {
+//     let isMounted = true
+//     fetchGraphQL(`
+//       query RepositoryNameQuery {
+//         # feel free to change owner/name here
+//         repository(owner: "beforegolive" name: "relay-start-demo") {
+//           name
+//         }
+//       }
+//     `)
+//       .then((response) => {
+//         // Avoid updating state if the component unmounted before the fetch completes
+//         if (!isMounted) {
+//           return
+//         }
+//         const data = response.data
+//         setName(data.repository.name)
+//       })
+//       .catch((error) => {
+//         console.error(error)
+//       })
+
+//     return () => {
+//       isMounted = false
+//     }
+//   }, [])
+
+//   // Render "Loading" until the query completes
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>{name != null ? `Repository: ${name}` : 'Loading'}</p>
+//       </header>
+//     </div>
+//   )
+// }
+
+// export default App
